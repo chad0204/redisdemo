08cd30649675cee466356d3e6a3fdc46c7ccb88c
pengchao <2389367080@qq.com>
2020-04-03 11:14:01 +0800

    jedis

--- redis-jedis-cluster/src/test/java/com/pc/redisjediscluster/RedisJedisClusterApplicationTests.java
+++ redis-jedis-cluster/src/test/java/com/pc/redisjediscluster/RedisJedisClusterApplicationTests.java
@@ -1,13 +1,27 @@
 package com.pc.redisjediscluster;
 
-import org.junit.jupiter.api.Test;
+import com.pc.redisjediscluster.jedis.JedisUtil;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class RedisJedisClusterApplicationTests {
+public class RedisJedisClusterApplicationTests {
+
+    @Autowired
+    private JedisUtil jedisUtil;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+
+
+        jedisUtil.set("ddd","111");
+
+        System.out.println(jedisUtil.get("ddd"));
+
     }
 
 }
